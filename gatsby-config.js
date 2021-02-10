407953e13c90df9bf472c0d255eca14a8623f392
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-10 13:43:34 +0200

    Add Google Fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,23 @@ module.exports = {
     description:
       "Alexandra Stoica's personal portfolio including work related to UX/Front End Engineering and Human-Computer Interaction.",
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Work sans`,
+            subsets: [`400`, `500`, `600`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `600`, `700`],
+          },
+        ],
+      },
+    },
+  ],
 };
