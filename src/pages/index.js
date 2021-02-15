5078c8386fa3d51ef3012e6c0ca284656e11ffa2
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-15 21:02:05 +0200

    Improve homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,22 @@ import Layout from '../components/layout';
 import ArticleCard from '../components/article-card';
 import Header from '../components/header';
 import useArticles from '../hooks/use-articles';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const CardWrapper = styled('li')`
+  position: relative;
+  text-align: left;
+  background-color: #fff;
+  padding: 32px;
+  border-radius: 2px;
+  transform: translateY(0);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 export default function Home() {
   const articles = useArticles();
@@ -14,9 +30,21 @@ export default function Home() {
         description="UX/Frontend Enginner @Potato London. MSc Human-Computer Interaction (Distinction) at UCL, London, UK. BSc Web Development (1:1 Hons) at WorcUni, UK. Interested in all things frontend and human-computer interaction. 🧠👩🏻‍💻"
       ></Header>
 
-      {articles.map((article) => (
-        <ArticleCard key={article.slug} article={article} />
-      ))}
+      <ul
+        css={css`
+          display: grid;
+          gap: 64px 32px;
+          grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+          padding: 0;
+          margin: 0;
+        `}
+      >
+        {articles.map((article, i) => (
+          <CardWrapper key={i}>
+            <ArticleCard article={article} />
+          </CardWrapper>
+        ))}
+      </ul>
     </Layout>
   );
 }
