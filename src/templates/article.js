5ecf87fe42f0284451a203916360ba850bb573c5
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-22 16:02:20 +0200

    Style article page

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
@@ -28,6 +27,36 @@ const FlexLayout = styled('div')`
   display: flex;
   padding: 0 88px;
   margin: 0;
+  width: 100%;
+`;
+
+const ArticleBody = styled('article')`
+  position: relative;
+  background: #fff;
+  padding-bottom: 88px;
+  padding-top: 88px;
+  box-sizing: border-box;
+  flex: 1;
+  padding: 88px 64px;
+  margin: 0 auto;
+
+  h1 + p {
+    color: #42555d;
+    font: 400 1.5rem/2rem Work Sans, sans-serif;
+  }
+
+  h2 {
+    margin: 56px 0 32px;
+  }
+`;
+
+const ArticleBodyCorner = styled('span')`
+  background: linear-gradient(to top left, #001e49 50%, #fdf6f3 0);
+  height: 50px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 50px;
 `;
 
 const ArticleTemplate = ({ data: { mdx: article } }) => (
@@ -35,30 +64,10 @@ const ArticleTemplate = ({ data: { mdx: article } }) => (
     <Breadcrumb title={article.frontmatter.title}></Breadcrumb>
     <FlexLayout>
       <Sidebar article={article} />
-      <article
-        css={css`
-          position: relative;
-          background: #fff;
-          padding-bottom: 88px;
-          padding-top: 88px;
-          box-sizing: border-box;
-          flex: 1;
-          padding: 88px 64px;
-          margin: 0 auto;
-        `}
-      >
-        <span
-          css={css`
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 50px;
-            height: 50px;
-            background: linear-gradient(to top left, #001e49 50%, #fdf6f3 0);
-          `}
-        ></span>
+      <ArticleBody>
+        <ArticleBodyCorner></ArticleBodyCorner>
         <MDXRenderer>{article.body}</MDXRenderer>
-      </article>
+      </ArticleBody>
     </FlexLayout>
   </Layout>
 );
