c5f0bd62f51d7f5ec079069485325a8b22dcdb1b
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-22 16:01:35 +0200

    Style article card component

--- src/components/article-card.js
+++ src/components/article-card.js
@@ -1,68 +1,75 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
 import { Link } from 'gatsby';
 
 const ArticleCardCorner = styled('span')`
-  background: linear-gradient(to bottom left, transparent 50%, #001e49 0);
+  background: linear-gradient(to bottom left, #e8fafd 50%, #001e49 0);
   height: 50px;
   position: absolute;
   right: 0;
   top: 0;
   width: 50px;
+  z-index: 1;
 `;
 
 const ArticleCardCTA = styled(Link)`
-  text-decoration: none;
-  color: #1d27ff;
   align-self: flex-end;
   border-radius: 100%;
   border: 2px solid #1d27ff;
   box-sizing: border-box;
+  color: #1d27ff;
   height: 35px;
   line-height: 35px;
   margin-left: -10px;
   text-align: center;
+  text-decoration: none;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   width: 35px;
+
+  .material-icons {
+    line-height: 32px;
+    font-size: inherit;
+  }
+`;
+
+const ArticleCardLabel = styled('div')`
+  font: 600 0.875rem/1.125rem Open sans, sans-serif;
+  letter-spacing: 1px;
+  margin-bottom: 24px;
+  text-transform: uppercase;
+  &::after {
+    background: #ffdb13;
+    content: ' ';
+    display: block;
+    height: 2px;
+    margin-top: 8px;
+    width: 80px;
+  }
 `;
 
 const ArticleCardWrapper = styled('div')`
-  position: relative;
-  text-align: left;
-  background-color: #fff;
-  padding: 32px;
-  border-radius: 2px;
-  transform: translateY(0);
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-  overflow: hidden;
   display: flex;
   flex-direction: column;
+  height: 100%;
   justify-content: space-between;
+  overflow: hidden;
+  text-align: left;
 `;
 
 const ArticleCard = ({ article }) => (
-  <ArticleCardWrapper>
+  <>
     <ArticleCardCorner />
-    <div
-      css={css`
-        margin-bottom: 24px;
-        &::after {
-          content: ' ';
-          width: 80px;
-          height: 2px;
-          margin-top: 8px;
-          background: #ffdb13;
-          display: block;
-        }
-      `}
-    >
-      {article.label}
-    </div>
-    <h3>{article.title}</h3>
-    <p>{article.preview}</p>
-    <ArticleCardCTA to={`articles/${article.slug}`}>&rarr;</ArticleCardCTA>
-  </ArticleCardWrapper>
+    <ArticleCardWrapper>
+      <ArticleCardLabel>{article.label}</ArticleCardLabel>
+      <h3>
+        <Link to={`articles/${article.slug}`}>{article.title}</Link>
+      </h3>
+      <p>{article.preview}</p>
+      <ArticleCardCTA to={`articles/${article.slug}`}>
+        <span className="material-icons">arrow_forward</span>
+      </ArticleCardCTA>
+    </ArticleCardWrapper>
+  </>
 );
 
 export default ArticleCard;
