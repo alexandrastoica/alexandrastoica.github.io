4a5bf9702afe8e0c608f6a2f1c53f9c012c30252
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-22 16:00:44 +0200

    Style breadcrumb component

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
 import { Link } from 'gatsby';
 
-const BreadcrumbBody = styled('nav')`
+const BreadcrumbWrapper = styled('nav')`
   background: #fdf6f3;
   position: sticky;
   z-index: 1;
@@ -11,11 +10,36 @@ const BreadcrumbBody = styled('nav')`
   top: 0;
 `;
 
+const BreadcrumbBody = styled('ul')`
+  align-items: center;
+  display: flex;
+  list-style: none;
+  margin: 0 88px;
+  padding: 32px 0 16px;
+
+  li {
+    font: 500 11px/11px Open Sans, sans-serif;
+    letter-spacing: 1px;
+    line-height: 2rem;
+    margin-top: 0;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-transform: uppercase;
+    white-space: nowrap;
+  }
+
+  .material-icons {
+    margin: 0 24px;
+  }
+`;
+
 const HomeLink = styled(Link)`
-  font: 500 1.5rem/1.6875rem Open Sans, sans-serif;
-  text-decoration: none;
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   color: #1d27ff;
+  font: 600 1.5rem/1.6875rem Open Sans, sans-serif;
+  letter-spacing: 0;
+  text-decoration: none;
+  text-transform: none;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   white-space: nowrap;
 
   &:active,
@@ -26,48 +50,15 @@ const HomeLink = styled(Link)`
 `;
 
 const Breadcrumb = ({ title }) => (
-  <BreadcrumbBody>
-    <ul
-      css={css`
-        align-items: center;
-        display: flex;
-        list-style: none;
-        margin: 0 88px;
-        padding: 32px 0 16px;
-      `}
-    >
-      <li
-        css={css`
-          white-space: nowrap;
-        `}
-      >
+  <BreadcrumbWrapper>
+    <BreadcrumbBody>
+      <li>
         <HomeLink to="/">Alexandra Stoica</HomeLink>
-        <span
-          css={css`
-            margin: 0 20px;
-          `}
-          className="material-icons md-18"
-        >
-          chevron_right
-        </span>
+        <span className="material-icons md-18">chevron_right</span>
       </li>
-      <li
-        css={css`
-          font: 500 11px/11px Open Sans, sans-serif;
-          margin-top: 0;
-          line-height: 2rem;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          width: 100%;
-          letter-spacing: 1px;
-          text-transform: uppercase;
-        `}
-      >
-        {title}
-      </li>
-    </ul>
-  </BreadcrumbBody>
+      <li>{title}</li>
+    </BreadcrumbBody>
+  </BreadcrumbWrapper>
 );
 
 export default Breadcrumb;
