64618b97f54e6c7fa3171a3fed7ada80729fe258
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-22 16:00:57 +0200

    Style sidebar component

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
 
 const SidebarBody = styled('aside')`
   background: #fdf6f3;
@@ -10,12 +9,23 @@ const SidebarBody = styled('aside')`
   max-width: 360px;
 `;
 
-const DT = styled('dt')`
-  border-top: 0.5px solid rgba(0, 0, 0, 0.06);
-  font: 400 0.875rem/1.125rem Open Sans, sans-serif;
-  letter-spacing: 1px;
-  padding-top: 32px;
-  text-transform: uppercase;
+const List = styled(`dl`)`
+  dt {
+    border-top: 0.5px solid rgba(0, 0, 0, 0.06);
+    font: 600 0.875rem/1.125rem Open Sans, sans-serif;
+    letter-spacing: 1px;
+    padding-top: 32px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+
+    &:first-child {
+      border: none;
+    }
+  }
+
+  dd {
+    font: 400 1rem/1.5rem Open Sans, sans-serif;
+  }
 `;
 
 const Button = styled('a')`
@@ -37,24 +47,33 @@ const Button = styled('a')`
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 `;
 
+const FillIcon = styled('span')`
+  background: #fff;
+  border-radius: 100px;
+  font-size: 28px;
+  line-height: 1;
+  margin-right: 8px;
+  padding: 10px;
+  vertical-align: middle;
+`;
+
 const Sidebar = ({ article }) => (
   <SidebarBody>
-    <dl>
-      <DT
-        css={css`
-          border: none;
-        `}
-      >
-        Date
-      </DT>
+    <List>
+      <dt>
+        <FillIcon className="material-icons">menu_book</FillIcon>
+        {article.frontmatter.label}
+      </dt>
+
+      <dt>Date</dt>
       <dd>{article.frontmatter.date}</dd>
 
-      <DT>Context</DT>
+      <dt>Context</dt>
       {article.frontmatter.context.map((cont, i) => (
-        <dd key={i}>{cont} </dd>
+        <dd key={i}>{cont}</dd>
       ))}
 
-      <DT>Links</DT>
+      <dt>Links</dt>
       {article.frontmatter.links.map((link, i) => (
         <dd key={i}>
           <Button href={link} target="_blank" rel="noreferrer">
@@ -63,11 +82,11 @@ const Sidebar = ({ article }) => (
         </dd>
       ))}
 
-      <DT>Languages</DT>
+      <dt>Languages</dt>
       {article.frontmatter.languages.map((lang, i) => (
         <dd key={i}>{lang}</dd>
       ))}
-    </dl>
+    </List>
   </SidebarBody>
 );
 
