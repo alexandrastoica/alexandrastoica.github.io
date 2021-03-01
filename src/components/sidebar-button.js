68e5f5e75381414f1412fb36d53a42959b577fb4
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-03-01 20:48:06 +0200

    nit: Order

--- src/components/sidebar-button.js
+++ src/components/sidebar-button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Button = styled('a')`
-  font: 500 1rem/1.5rem Open Sans, sans-serif;
   align-items: center;
   background-color: #1d27ff;
   border-radius: 24px;
@@ -10,14 +9,15 @@ const Button = styled('a')`
   color: #fff;
   cursor: pointer;
   display: flex;
+  font: 500 1rem/1.5rem Open Sans, sans-serif;
   justify-content: center;
   letter-spacing: 1px;
   min-height: 3rem;
   padding: 0 2rem;
   text-decoration: none;
   text-rendering: optimizeLegibility;
-  width: fit-content;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  width: fit-content;
 
   span {
     margin-right: 8px;
@@ -29,6 +29,10 @@ const Button = styled('a')`
     background: #0e17da;
     box-shadow: 0 0.2rem 0 rgb(0 0 0 / 20%);
     text-decoration: none;
+  }
+
+  &:active {
+    box-shadow: none;
   }
 `;
 
