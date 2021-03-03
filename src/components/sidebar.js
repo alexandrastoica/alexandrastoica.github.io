6c63fabac2d65c92ee39a6b6d43c1e1e8ab4c0bb
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-03-03 19:49:56 +0200

    Finish sidebar component

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import SidebarButton from './sidebar-button';
 
 const SidebarBody = styled('aside')`
   background: #fdf6f3;
   box-sizing: border-box;
   flex: 0 0 360px;
-  padding: 88px 32px 88px 0;
   max-width: 360px;
+  padding: 88px 32px 88px 0;
 `;
 
 const List = styled(`dl`)`
@@ -14,9 +15,9 @@ const List = styled(`dl`)`
     border-top: 0.5px solid rgba(0, 0, 0, 0.06);
     font: 600 0.875rem/1.125rem Open Sans, sans-serif;
     letter-spacing: 1px;
+    letter-spacing: 1px;
     padding-top: 32px;
     text-transform: uppercase;
-    letter-spacing: 1px;
 
     &:first-child {
       border: none;
@@ -28,23 +29,14 @@ const List = styled(`dl`)`
   }
 `;
 
-const Button = styled('a')`
-  font: 500 1rem/1.5rem Open Sans, sans-serif;
-  align-items: center;
-  background-color: #1d27ff;
-  border-radius: 24px;
-  box-shadow: 0 0.125rem 0 rgb(0 0 0 / 20%);
-  color: #fff;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  letter-spacing: 1px;
-  min-height: 3rem;
-  padding: 0 2rem;
-  text-decoration: none;
-  text-rendering: optimizeLegibility;
+const Tag = styled('dd')`
+  border-radius: 8px;
+  border: 1px solid;
+  float: left;
+  font: 400 0.725rem/1rem Open Sans, sans-serif !important;
+  margin-right: 8px;
+  padding: 4px 8px;
   width: fit-content;
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 `;
 
 const FillIcon = styled('span')`
@@ -57,37 +49,46 @@ const FillIcon = styled('span')`
   vertical-align: middle;
 `;
 
-const Sidebar = ({ article }) => (
-  <SidebarBody>
-    <List>
-      <dt>
-        <FillIcon className="material-icons">menu_book</FillIcon>
-        {article.frontmatter.label}
-      </dt>
+class Sidebar extends React.Component {
+  render() {
+    const article = this.props.article;
+    const label = article.frontmatter.label;
+    const date = article.frontmatter.date;
+    const context = article.frontmatter.context;
+    const links = article.frontmatter.links;
+    const keywords = article.frontmatter.keywords;
 
-      <dt>Date</dt>
-      <dd>{article.frontmatter.date}</dd>
+    return (
+      <SidebarBody>
+        <List>
+          <dt>
+            <FillIcon className="material-icons">menu_book</FillIcon>
+            {label}
+          </dt>
 
-      <dt>Context</dt>
-      {article.frontmatter.context.map((cont, i) => (
-        <dd key={i}>{cont}</dd>
-      ))}
+          <dt>Date</dt>
+          <dd>{date}</dd>
 
-      <dt>Links</dt>
-      {article.frontmatter.links.map((link, i) => (
-        <dd key={i}>
-          <Button href={link} target="_blank" rel="noreferrer">
-            <span className="material-icons">open_in_new</span>Source
-          </Button>
-        </dd>
-      ))}
+          <dt>Context</dt>
+          {context.map((cont, i) => (
+            <dd key={i}>{cont}</dd>
+          ))}
 
-      <dt>Languages</dt>
-      {article.frontmatter.languages.map((lang, i) => (
-        <dd key={i}>{lang}</dd>
-      ))}
-    </List>
-  </SidebarBody>
-);
+          <dt>Links</dt>
+          {links.map((link, i) => (
+            <dd key={i}>
+              <SidebarButton link={link}></SidebarButton>
+            </dd>
+          ))}
+
+          <dt>Keywords</dt>
+          {keywords.map((lang, i) => (
+            <Tag key={i}>{lang}</Tag>
+          ))}
+        </List>
+      </SidebarBody>
+    );
+  }
+}
 
 export default Sidebar;
